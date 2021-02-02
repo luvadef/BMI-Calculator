4a66da8488569daeaf90fb9eef5eafaeedd77b49
Luis Valdes <luisvaldes@Luiss-MacBook-Pro.local>
2021-02-02 18:58:37 -0300

    first commit

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,21 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Luis Valdes on 02-02-21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+    init(value: Float, advice: String, color: UIColor) {
+        self.value = value
+        self.advice = advice
+        self.color = color
+    }
+}
---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,38 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Luis Valdes on 02-02-21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    var bmi: BMI?
+    
+    func getBMIValue() -> String{
+        let bmiTo1DecimalPlace = String(format: "%.1f",  bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No Advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+    mutating func calculateBMI(_ weight:Float, _ height:Float){
+        let bmiValue = weight / pow(height, 2)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+        }else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        }else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        }
+        
+    }
+}
